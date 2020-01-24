77aeb60437676097f7af9a954cea7adeb4618197
JM <janfelicilda@gmail.com>
2020-01-24 15:31:56 +0800

    test

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{$title}}</h1>
+    <h1><?php echo $title ?></h1>
     <p>
-        sample branch
+        Sample Branch wowowin
     </p>
 @endsection
-
+    
