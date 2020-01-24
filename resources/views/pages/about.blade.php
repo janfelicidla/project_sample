dcebc99a43dd4d7e7bd8d51ef9332a70501d997d
JM <janfelicilda@gmail.com>
2020-01-24 13:51:05 +0800

    asd

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,11 +3,7 @@
 @section('content')
     <h1>{{$title}}</h1>
     <p>
-<<<<<<< HEAD
         asefasefasefasefase
-=======
-    sample changes.aasdasd
->>>>>>> fbfb67e77cf46869dbccf4e825560fd68030c6a8
     </p>
 @endsection
 
