31067f867bae3446a8088a352dacf658ee511036
JM <janfelicilda@gmail.com>
2020-01-24 10:40:43 +0800

    asd

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>
+    This is the larave application from the "Laravel From Scratch Youtube Series".
+    </p>
+@endsection
